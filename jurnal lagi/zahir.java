981076ccfdc7964b340e4589f74505482918c8f1
muhammad zahir abdullah <zahirabdullah@student.telkomuniversity.ac.id>
2020-09-17 14:11:25 -0800

    zahir.java

---
+++ jurnal lagi/zahir.java
@@ -0,0 +1,10 @@
+public class zahir {
+
+	public static void main (String[] args){
+		System.out.println("Nama	: Muhammad Zahir Abdullah");
+		System.out.println("NIM	: 6706202114");
+		System.out.println("Kelas	: D3IF 44-03");
+		
+		
+	}
+}
